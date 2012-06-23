7730098f52a69799a69533ebea561195a21b2872
Anthony <anthonyarr@gmail.com>
2012-06-23 15:00:52 -0700

    trying to deploy

---
+++ aca/config.py
@@ -0,0 +1,16 @@
+DB_NAME = 'thenewworld9'
+DB_USER = 'apa'
+DB_PASS = 'thenewworld9'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'anthonyarr@gmail.com'
+EMAIL_HOST_PASSWORD = 'RedeCorreio3.'
+
+
+
+
+
