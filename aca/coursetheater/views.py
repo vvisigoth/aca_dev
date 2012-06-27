f16cce33af6c22b4c37e646b3d6a3160ced8b27a
Anthony <anthonyarr@gmail.com>
2012-06-26 20:27:39 -0700

    added some @login_required

--- aca/coursetheater/views.py
+++ aca/coursetheater/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     return render_to_response('coursetheater/coursetheater.html', context_instance=RequestContext(request))
