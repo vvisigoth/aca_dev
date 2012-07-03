7e7ffd2e462578df32859d9ed263f7054c13b65a
Anthony <anthonyarr@gmail.com>
2012-07-02 17:25:40 -0700

    edited Procfile

--- aca/answerbase/tasks.py
+++ aca/answerbase/tasks.py
@@ -2,6 +2,7 @@ from datetime import date, timedelta
 from celery.task import Task, PeriodicTask
 from answerbase.models import Question
 
+"""
 class TestTask(PeriodicTask):
     run_every = timedelta(seconds=60)
 
@@ -9,3 +10,4 @@ class TestTask(PeriodicTask):
         logger = self.get_logger(**kwargs)
         logger.info("Running Test Task")
         return True
+"""
