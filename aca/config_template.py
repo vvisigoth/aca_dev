62fbc982d94cf3dfd2b9dd4e140b9e9fc5407e0a
anthonyarroyo <anthonyarr@gmail.com>
2012-12-03 18:54:18 -0600

    deleted some sensitive files

---
+++ aca/config_template.py
@@ -0,0 +1,26 @@
+DB_NAME = ''
+DB_USER = ''
+DB_PASS = ''
+
+#gmail settings
+
+"""
+EMAIL_HOST = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+"""
+#production settings
+
+EMAIL_HOST = ''
+EMAIL_PORT = 26
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+
+
+
+
